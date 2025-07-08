63e6718a1382be215f9f1c9e9912aa741118e1a3
José Francisco Ruso Álvarez <139748836+Jose-Ruso-NTT@users.noreply.github.com>
2025-07-08 20:22:31 +0200

    feat: add app initializer and environment initializer to app configuration (#1)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {
+  ApplicationConfig,
+  provideAppInitializer,
+  provideEnvironmentInitializer,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import {provideRouter, withRouterConfig} from '@angular/router';
 
 import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
+import {of} from 'rxjs';
 import {routes} from './app.routes';
 import {authInterceptor} from './core/interceptors';
 
@@ -10,5 +16,13 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes, withRouterConfig({})),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideAppInitializer(() => {
+      // This is where you can initialize your app, e.g., load configuration or check authentication
+      return of(console.log('App initialized'));
+    }),
+    provideEnvironmentInitializer(() => {
+      // This is where you can set up environment-specific configurations
+      console.log('Environment initialized');
+    }),
   ],
 };
